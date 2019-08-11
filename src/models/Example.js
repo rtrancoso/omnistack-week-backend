f34d194c9408e625df94b375f0a84bae799ab268
Rodrigo Trancoso <rodrigo.trancoso@itixti.com.br>
2019-08-10 22:01:04 -0300

    updates

--- src/models/Example.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const ExampleSchema = new Schema({
-
-}, { timestamps: true });
-
-module.exports = model('Example', ExampleSchema);
--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const server = express();
 
-mongoose.connect('', {
+mongoose.connect('mongodb+srv://omnistack-user-db:W9FAyaTvYks0P8Pc@cluster0-h4uvn.mongodb.net/omnistack-week?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 

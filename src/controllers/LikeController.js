e5f824fffca48b0b51ed7922ae1d15b1e7b410c6
Rodrigo Trancoso <rodrigo.trancoso@itixti.com.br>
2019-08-12 00:20:05 -0300

    updates

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -13,7 +13,16 @@ module.exports = {
         }
 
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('Deu match!');
+            const loggedSocket = req.connectedDevs[loggedDev._id];
+            const targetSocket = req.connectedDevs[targetDev._id];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
--- src/server.js
+++ src/server.js
@@ -4,14 +4,30 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedDevs = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    connectedDevs[user] = socket.id
+});
 
 mongoose.connect('mongodb+srv://omnistack-user-db:W9FAyaTvYks0P8Pc@cluster0-h4uvn.mongodb.net/omnistack-week?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedDevs = connectedDevs;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
